b25fedd85a43cc0be1a1aa9a7e45e49293472df4
Manoz <kevin@manoz.fr>
2013-07-22 17:25:19 +0200

    Improve search form and 404 page

--- manozouille/404.php
+++ manozouille/404.php
@@ -2,7 +2,7 @@
 	<div id="main" role="main">
 		<div class="grid grid-pad">
 			<section class="col-9-12" id="articles">
-				<div class="not-found">
+				<div class="not-found" role="search">
 					<h4><?php _e("It seems you couldn't find what you are looking for."); ?></h4>
 					<div id="triste"></div>
 					<p><?php _e('Perhaps you can try searching.'); ?></p>
